3ff16e22030cbc0f7432f81b3f962fcf0b2774a0
David Epps <depps@Davids-MacBook-Air.local>
2018-08-27 16:37:46 -0600

    merged previous CRAp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl'
+import Main from './components/main'
+import { Link, Route } from 'react-router-dom';
+
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className="demo-big-content">
+    <Layout>
+        <Header class="header-color" title="PROJECTS by David A. Epps" scroll>
+            <Navigation>
+                <Route exact path={`/home`} render={ (routerProps) => < Main routerProps={routerProps} setUpGame={this.setUpGame} />} />
+                <Link to="#introduction">Introduction</Link>
+                <Link to="#services">Services</Link>
+
+                <Link to="#contact">Contact</Link>
+            </Navigation>
+        </Header>
+        <Drawer title="Projects">
+            <Navigation className="drawer-nav">
+                <a href="/">
+                  HTML/CSS/JavaScript</a>
+                <a href="/">jQuery/Express/MongoDB</a>
+                <a href="/">RESTful.CRUD/React/Django</a>
+                <a href="/">Full Stack</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main />
+        </Content>
+    </Layout>
+</div>
     );
   }
 }
